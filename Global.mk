8f655910d8fbf96bc059a5e93ff47fe8c41f8b8d
John Adler <John_Adler@brown.edu>
2014-05-20 12:18:16 -0400

    different gdc flags for removing runtime library

--- Global.mk
+++ Global.mk
@@ -8,7 +8,7 @@ CSCOPE    := cscope
 CFLAGS    += -fno-builtin -nostdinc -ansi -g3 -fno-stack-protector -m32 -march=i686 -fsigned-char -Iinclude
 CFLAGS    += -Wall -Wredundant-decls -Wundef -Wpointer-arith -Wfloat-equal -Wnested-externs -Wvla -Winline -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable
 ASFLAGS   := -D__ASSEMBLY__
-DFLAGS += -nostdlib -nodefaultlibs -g -m32 -march=i686
+DFLAGS += -nophoboslib -g -m32 -march=i686
 
 ###
 
